6172d34369d57d75305793942002465134ab2617
Shane Wolsey <shane.wolsey@gmail.com>
2022-01-13 15:20:56 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = commands.Bot(command_prefix='$')
 
 @bot.command()
 async def embed(ctx):
-    channel = discord.utils.get(ctx.guild.channels, name=general)
+    channel = discord.utils.get(ctx.guild.channels, name="general")
     channel_id=channel.id
     rulesdesc = "**RULES**\n\nNo spam - `<#${channel_id}>`moderators will not tolerate spam\nNo advertising or affiliate links without ADMIN permission\nBe respectful at all times, kindness is required\nNo doxxing\nPlease use the right channels\nNo discussion of illegal practices\nReport any malicious behaviours to the moderators\nNo pump and dumps"
     embed=discord.Embed(title="Welcome to the Discord server for Perth Crypto Crew", description=''.join(rulesdesc), color=0xeb0057)
