1526353e751715f56d0f015f47a8f9bdb0c9c8fd
Shane Wolsey <shane.wolsey@gmail.com>
2022-01-13 17:56:53 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = commands.Bot(command_prefix='$')
 
 @bot.command()
 async def embed(ctx):
-    channel = discord.utils.get(ctx.guild.channels, name="🤖-bot-commands")
+    channel = discord.utils.get(ctx.guild.channels, name="rules")
     embed=discord.Embed(title="Welcome to the Discord server for Perth Crypto Crew", description=f"**RULES**\n\nNo spam - moderators will not tolerate spam\nNo advertising or affiliate links without ADMIN permission\nBe respectful at all times, kindness is required\nNo doxxing\nPlease use the right channels\nNo discussion of illegal practices\nReport any malicious behaviours to the moderators\nNo pump and dumps\n\nIf you cannot see the other channels, please go to {channel.mention} and type /verify (you will need to allow DMs from this server).", color=0xeb0057)
     await ctx.send(embed=embed)
 
