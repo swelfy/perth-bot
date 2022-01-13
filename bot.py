8ed99feae9501dd4ed96e767ddefdbf121b8cdde
Shane Wolsey <shane.wolsey@gmail.com>
2022-01-13 12:29:24 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,15 @@ import discord
 from discord.ext import commands
 import os
 
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
 @bot.command()
 async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
