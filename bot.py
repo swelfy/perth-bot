bba27d5eb3d2fff97c5114ff2ec14cc0b76dd45c
Shane Wolsey <shane.wolsey@gmail.com>
2022-01-13 13:51:22 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,4 +4,9 @@ import os
 
 bot = commands.Bot(command_prefix='$')
 
+@bot.command()
+async def test(ctx):
+    description="test result"
+await ctx.send(description)
+
 bot.run(os.environ["DISCORD_TOKEN"])
