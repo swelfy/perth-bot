86484f40d6142323ddb68eacd1050f5bdac0e3cb
Shane Wolsey <shane.wolsey@gmail.com>
2022-01-13 13:04:01 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ bot = commands.Bot(command_prefix='$')
 
 @bot.command()
 async def foo(ctx, arg):
-    await ctx.send(arg)
+    embed=discord.Embed(title="Sample Embed", url="https://realdrewdata.medium.com/", description="This is an embed that will show how to build an embed and the different components", color=discord.Color.blue())
+    await ctx.send(embed)
 
 bot.run(os.environ["DISCORD_TOKEN"])
