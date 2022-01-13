3f24ceca814c0832724be71214ace3b8f7ee9505
Shane Wolsey <shane.wolsey@gmail.com>
2022-01-13 14:19:19 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,15 +6,7 @@ bot = commands.Bot(command_prefix='$')
 
 @bot.command()
 async def embed(ctx):
-    embed=discord.Embed(title="Welcome to the Discord server for Perth Crypto Crew", color=0xeb0057)
-    embed.add_field(name="", value="No spam - moderators will not tolerate spam", inline=False)
-    embed.add_field(name="", value="No advertising or affiliate links without ADMIN permission", inline=False)
-    embed.add_field(name="", value="Be respectful at all times, kindness is required", inline=False)
-    embed.add_field(name="", value="No doxxing", inline=False)
-    embed.add_field(name="", value="Please use the right channels", inline=False)
-    embed.add_field(name="", value="No discussion of illegal practices", inline=False)
-    embed.add_field(name="", value="Report any malicious behaviours to the moderators", inline=False)
-    embed.add_field(name="", value="No pump and dumps", inline=False)
+    embed=discord.Embed(title="Welcome to the Discord server for Perth Crypto Crew", description="No spam - moderators will not tolerate spam\nNo advertising or affiliate links without ADMIN permission\nBe respectful at all times, kindness is required\nNo doxxing\nPlease use the right channels\nNo discussion of illegal practices\nReport any malicious behaviours to the moderators\nNo pump and dumps", color=0xeb0057)
     await ctx.send(embed=embed)
 
 bot.run(os.environ["DISCORD_TOKEN"])
