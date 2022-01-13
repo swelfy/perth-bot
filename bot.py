04e69b2b71149b6d11f6da258b8b38eb4d095337
Shane Wolsey <shane.wolsey@gmail.com>
2022-01-13 12:47:04 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,21 +2,10 @@ import discord
 from discord.ext import commands
 import os
 
-description = '''An example bot to showcase the discord.ext.commands extension
-module.
-There are a number of utility commands being showcased here.'''
-
-intents = discord.Intents.default()
-intents.members = True
-
-bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+bot = commands.Bot(command_prefix='$')
 
 @bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
-
-client = discord.Client()
-client.run(os.environ["DISCORD_TOKEN"])
+async def foo(ctx, arg):
+    await ctx.send(arg)
 
 bot.run(os.environ["DISCORD_TOKEN"])
