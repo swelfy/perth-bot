5308fdb3acd6d3c2fd5029733f73216009bf87ea
Shane Wolsey <shane.wolsey@gmail.com>
2022-01-13 15:05:27 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,8 @@ import os
 
 bot = commands.Bot(command_prefix='$')
 
-rulesdesc = "**RULES**\n\nNo spam - moderators will not tolerate spam\nNo advertising or affiliate links without ADMIN permission\nBe respectful at all times, kindness is required\nNo doxxing\nPlease use the right channels\nNo discussion of illegal practices\nReport any malicious behaviours to the moderators\nNo pump and dumps"
+channel = discord.utils.get(ctx.guild.channels, name='general')
+rulesdesc = "**RULES**\n\nNo spam - {channel}moderators will not tolerate spam\nNo advertising or affiliate links without ADMIN permission\nBe respectful at all times, kindness is required\nNo doxxing\nPlease use the right channels\nNo discussion of illegal practices\nReport any malicious behaviours to the moderators\nNo pump and dumps"
 
 @bot.command()
 async def embed(ctx):
