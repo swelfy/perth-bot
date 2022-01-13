82f621f1e51d1ae1e35c5c3e41eff1a2ef03bf79
Shane Wolsey <shane.wolsey@gmail.com>
2022-01-13 12:22:01 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,12 @@
 import discord
+from discord.ext import commands
 import os
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
 
 client = discord.Client()
 client.run(os.environ["DISCORD_TOKEN"])
+
